2291821227089fd962c563a63d6f4d10d8171ee6
Andrew Li <andrewhanks1113@gmail.com>
2022-08-08 17:08:58 +0800

    Add another solution for question 1248

--- src/main/kotlin/questions/Question_1248_Count_Number_of_Nice_Subarrays.kt
+++ src/main/kotlin/questions/Question_1248_Count_Number_of_Nice_Subarrays.kt
@@ -44,5 +44,53 @@ class Question_1248_Count_Number_of_Nice_Subarrays {
             }
             return result
         }
+
+        fun numberOfSubarraysPointSolution(nums: IntArray, k: Int): Int {
+            var i = 0
+            var j = 0
+            var odd = 0
+            var result = 0
+            var temp = 0
+
+            /*
+            Approach : two pointer + sliding window technique
+
+            step 1 : we have fix i and moving j until our count of odd numbers == k
+            step 2 : when(odd == count) we are counting every possible subarray by reducing the size of subarray from i
+
+            why temp?
+            from reducing the size of subarray we will count all the possible subarray from between i and j
+            but when i encounter a odd element the odd count will reduce and that while will stop executing
+
+            now there are two possible cases
+            1.The leftover elements have a odd number
+            2.The leftover elements do not have any odd number
+
+            1. if our leftover elements have a odd number
+                    then we cannot include our old possible subarrays into new possible subarrays because now new window for having odd == k is formed
+                    that's why temp = 0;
+
+            2. if out leftover elements do not have any odd element left
+                then our leftover elements must also take in consideration becuase they will also contribute in forming subarrays
+            */
+
+            while (j < nums.size) {
+                if (nums[j] % 2 != 0) {
+                    odd++
+                    //if leftover elements have odd element then new window is formed so we set temp = 0;
+                    temp = 0
+                }
+                while (odd == k) {
+                    temp++
+                    if (nums[i] % 2 != 0) odd--
+                    i++
+                }
+                //if no leftover elements is odd, each element will part in forming subarray
+                //so include them
+                result += temp
+                j++
+            }
+            return result
+        }
     }
 }
