d378f043393ad0f28221add8f1cb3c1b125d5753
Andrewhanks <andrewhanks1113@gmail.com>
2023-09-17 18:28:25 +0800

    Add question 1266

---
+++ src/main/kotlin/questions/Question_1266_Minimum_Time_Visiting_All_Points.kt
@@ -0,0 +1,25 @@
+package questions
+
+
+class Question_1266_Minimum_Time_Visiting_All_Points {
+
+    companion object {
+
+        fun runQuestion() {
+            val intArray = arrayOf(intArrayOf(1, 1), intArrayOf(3, 4), intArrayOf(-1, 0))
+            val result = minTimeToVisitAllPoints(intArray)
+            println("Question 1266: $result")
+        }
+
+        fun minTimeToVisitAllPoints(points: Array<IntArray>): Int {
+            var result = 0
+            for (count in 0..points.size - 2) {
+                result += Math.max(
+                    Math.abs(points[count][0] - points[count + 1][0]),
+                    Math.abs(points[count][1] - points[count + 1][1])
+                )
+            }
+            return result
+        }
+    }
+}
