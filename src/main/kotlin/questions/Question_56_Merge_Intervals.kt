45ef02a03d045b994886dbaff38170256fea22a1
Andrew Li <ali@bn.com>
2023-10-02 16:32:05 +0800

    Add question 56

---
+++ src/main/kotlin/questions/Question_56_Merge_Intervals.kt
@@ -0,0 +1,52 @@
+package questions
+
+import kotlin.math.max
+
+
+class Question_56_Merge_Intervals {
+
+    companion object {
+
+        fun runQuestion() {
+            val intArray = arrayOf(intArrayOf(2, 3), intArrayOf(1, 4))
+            val resultList = merge(intArray)
+            var result = "["
+            resultList.forEachIndexed { firstIndex, firstI ->
+                resultList[firstIndex].forEachIndexed { secondIndex, secondI ->
+                    if (0 == secondIndex) {
+                        result += "["
+                    }
+                    result += secondI
+                    if (secondIndex == resultList[firstIndex].size - 1) {
+                        result += "]"
+                    } else {
+                        result += ","
+                    }
+                }
+                if (firstIndex == resultList.size - 1) {
+                    result += "]"
+                } else {
+                    result += ","
+                }
+            }
+            if (']' != result.last()) {
+                result += "]"
+            }
+            println("Question 1260: $result")
+        }
+
+        fun merge(intervals: Array<IntArray>): Array<IntArray> {
+            val result: MutableList<IntArray> = mutableListOf()
+            intervals.sortBy { it[0] }
+            result.add(intArrayOf(intervals[0][0], intervals[0][1]))
+            for (count in 1..intervals.size - 1) {
+                if (result[result.size - 1][1] >= intervals[count][0]) {
+                    result[result.size - 1][1] = max(result[result.size - 1][1], intervals[count][1])
+                } else {
+                    result.add(intArrayOf(intervals[count][0], intervals[count][1]))
+                }
+            }
+            return result.toTypedArray()
+        }
+    }
+}
