296a219e4663ebf0026502f708b46a628ac40d44
Andrew Li <andrewhanks1113@gmail.com>
2023-10-25 10:23:59 +0800

    Add question 39

---
+++ src/main/kotlin/questions/Question_39_Combination_Sum.kt
@@ -0,0 +1,72 @@
+package questions
+
+
+class Question_39_Combination_Sum {
+
+    companion object {
+
+        fun runQuestion() {
+//            Input: candidates = [2,3,5], target = 8
+//            Output: [[2,2,2,2],[2,3,3],[3,5]]
+            val candidates = intArrayOf(2, 3, 5)
+            val target = 8
+            val resultList = combinationSum(candidates, target)
+            var result = "["
+            resultList.forEachIndexed { firstIndex, firstI ->
+                resultList[firstIndex].forEachIndexed { secondIndex, secondI ->
+                    if (0 == secondIndex) {
+                        result += "["
+                    }
+                    result += secondI
+                    if (secondIndex == resultList[firstIndex].size - 1) {
+                        result += "]"
+                    } else {
+                        result += ","
+                    }
+                }
+                if (firstIndex == resultList.size - 1) {
+                    result += "]"
+                } else {
+                    result += ","
+                }
+            }
+            if (']' != result.last()) {
+                result += "]"
+            }
+            println("Question 39: $result")
+        }
+
+        fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
+            val result: MutableList<MutableList<Int>> = mutableListOf()
+            val temp: MutableList<Int> = mutableListOf()
+            dp(candidates.sorted().toIntArray(), target, 0, result, temp)
+            return result
+        }
+
+        fun dp(
+            candidates: IntArray,
+            target: Int,
+            index: Int,
+            result: MutableList<MutableList<Int>>,
+            temp: MutableList<Int>
+        ) {
+            if (target == 0) {
+                result.add(ArrayList(temp))
+//                val tempResult:MutableList<Int> = mutableListOf()
+//                temp.forEach { tempResult.add(it) }
+//                result.add(tempResult)
+                return
+            }
+            for (count in index..candidates.size - 1) {
+                val current = candidates[count]
+                if (target - current < 0) {
+                    return
+                }
+                println("target = $target, count = $count, current = $current, result = $result, temp = $temp")
+                temp.add(current)
+                dp(candidates, target - current, count, result, temp)
+                temp.removeAt(temp.size - 1)
+            }
+        }
+    }
+}
