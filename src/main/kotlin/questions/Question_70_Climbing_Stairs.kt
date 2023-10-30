a9a7c606c0a3e786e39c9d74246bf9451d51b45f
Andrew Li <andrewhanks1113@gmail.com>
2023-10-30 14:45:56 +0800

    Add question 70

---
+++ src/main/kotlin/questions/Question_70_Climbing_Stairs.kt
@@ -0,0 +1,54 @@
+package questions
+
+class Question_70_Climbing_Stairs {
+
+    companion object {
+
+        fun runQuestion() {
+//            Input: n = 45
+//            Output: 1836311903
+            val input = 45
+            val result = climbStairs(input)
+            println("Question 70: $result")
+        }
+
+        fun climbStairs(n: Int): Int {
+            if (n <= 2) {
+                return n
+            }
+            val record: MutableList<Int> = MutableList(n + 1) { -1 }
+            record[0] = 0
+            record[1] = 1
+            record[2] = 2
+            for (count in 3..n) {
+                record[count] = record[count - 1] + record[count - 2]
+            }
+            return record[n]
+        }
+
+        fun climbStairsDp(n: Int): Int {
+            val record: MutableList<Int> = MutableList(n + 1) { -1 }
+            record[0] = 0
+            record[1] = 1
+            record[2] = 2
+            return dp(n, record)
+        }
+
+        fun dp(n: Int, record: MutableList<Int>): Int {
+            if (n == 0) {
+                return 0
+            }
+            if (n == 1) {
+                return 1
+            }
+            if (n == 2) {
+                return 2
+            }
+            if (record[n] != -1) {
+                return record[n]
+            }
+            record[n] = dp(n - 1, record) + dp(n - 2, record)
+            return record[n]
+        }
+    }
+}
