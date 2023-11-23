c5b23bd7a2e4cfc7a44405c98db6ad038d9a6944
Andrew Li <andrewhanks1113@gmail.com>
2023-11-23 11:12:00 +0800

    Add question 129

---
+++ src/main/kotlin/questions/Question_129_Sum_Root_to_Leaf_Numbers.kt
@@ -0,0 +1,68 @@
+package questions
+
+import models.TreeNode
+
+
+class Question_129_Sum_Root_to_Leaf_Numbers {
+
+    companion object {
+
+        fun runQuestion() {
+//            Input: root = [4,9,0,5,1]
+//            Output: 1026
+            val root: TreeNode
+            val treeNode1 = TreeNode(4)
+            val treeNode2 = TreeNode(9)
+            val treeNode3 = TreeNode(0)
+            val treeNode4 = TreeNode(5)
+            val treeNode5 = TreeNode(1)
+            treeNode1.left = treeNode2
+            treeNode1.right = treeNode3
+            treeNode2.left = treeNode4
+            treeNode2.right = treeNode5
+            root = treeNode1
+            val result = sumNumbers(root)
+            println("Question 129: $result")
+        }
+
+        var sum = 0
+        fun sumNumbers(root: TreeNode?): Int {
+            if (root == null) {
+                return 0
+            }
+            val result: MutableList<Int> = mutableListOf()
+            preorder(root, result)
+            return sum
+        }
+
+        fun preorder(root: TreeNode?, result: MutableList<Int>) {
+            result.add(root?.`val`!!)
+            if (root?.left != null) {
+                preorder(root?.left, result)
+            }
+            if (root?.right != null) {
+                preorder(root?.right, result)
+            }
+            if (root?.left == null && root?.right == null) {
+                var pathNumber = 0
+                for (count in result.size - 1 downTo 0) {
+                    var tempNumber = result[count]
+                    for (x in 1..result.size - 1 - count) {
+                        tempNumber = tempNumber * 10
+                    }
+                    pathNumber += tempNumber
+                }
+                println("pathNumber = $pathNumber")
+                sum += pathNumber
+                if (!result.isEmpty()) {
+                    result.removeAt(result.size - 1)
+                }
+            }
+            if (root?.left != null || root?.right != null) {
+                if (!result.isEmpty()) {
+                    result.removeAt(result.size - 1)
+                }
+            }
+        }
+    }
+}
