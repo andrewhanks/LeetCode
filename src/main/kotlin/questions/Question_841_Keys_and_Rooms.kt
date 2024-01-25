bbaad010a0ccd18927c82baffb8c838b25e07b68
Andrew Li <andrewhanks1113@gmail.com>
2024-01-25 16:54:28 +0800

    Add question 841 solution 2

--- src/main/kotlin/questions/Question_841_Keys_and_Rooms.kt
+++ src/main/kotlin/questions/Question_841_Keys_and_Rooms.kt
@@ -33,5 +33,24 @@ class Question_841_Keys_and_Rooms {
             val allUnlocked = (unlocked.filter { it }.count() == unlocked.size)
             return allUnlocked
         }
+
+        fun canVisitAllRoomsWithRecursion(rooms: List<List<Int>>): Boolean {
+            val unlocked: MutableList<Boolean> = MutableList(rooms.size) { false }
+            unlocked[0] = true
+            bfs(rooms, rooms[0], unlocked)
+            val allUnlocked = (unlocked.filter { it }.count() == unlocked.size)
+            return allUnlocked
+        }
+
+        fun bfs(rooms: List<List<Int>>, keys: List<Int>, unlocked: MutableList<Boolean>) {
+            for (key in keys) {
+                if (unlocked[key]) {
+                    continue
+                }
+                unlocked[key] = true
+                println("key = $key")
+                bfs(rooms, rooms[key], unlocked)
+            }
+        }
     }
 }
