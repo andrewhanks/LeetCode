ddf8f1be2a9af4acca20174a052e5b27f301783a
Andrew Li <andrewhanks1113@gmail.com>
2024-02-27 14:39:13 +0800

    Add other solutions for 209

--- src/main/kotlin/questions/Question_209_Minimum_Size_Subarray_Sum.kt
+++ src/main/kotlin/questions/Question_209_Minimum_Size_Subarray_Sum.kt
@@ -15,6 +15,54 @@ class Question_209_Minimum_Size_Subarray_Sum {
             println("Question 209: $result")
         }
 
+        fun minSubArrayLenBaseEnd(target: Int, nums: IntArray): Int {
+            var length = nums.size
+            var sum = 0
+            var start = 0
+            for (end in 0..nums.size - 1) {
+                sum = nums[end]
+                start = end
+                while (sum < target && start > 0) {
+                    start--
+                    sum += nums[start]
+                }
+                //println("end = $end, start = $start, length = $length, sum = $sum")
+                if (sum >= target) {
+                    length = min(length, end - start + 1)
+                }
+            }
+            return if (length == nums.size && sum < target) {
+                0
+            } else {
+                length
+            }
+        }
+
+        fun minSubArrayLenWithAddFirst(target: Int, nums: IntArray): Int {
+            val result = IntArray(nums.size + 1)
+            result[0] = 0
+            for (count in 1..nums.size) {
+                result[count] = result[count - 1] + nums[count - 1]
+            }
+            var start = 0
+            var end = 0
+            var ans = Int.MAX_VALUE
+            while (end <= result.size - 1) {
+                if (result[end] - result[start] < target) {
+                    end++
+                } else {
+                    if (result[end] - result[start + 1] < target) {
+                        ans = min(ans, end - start)
+                    }
+                    start++
+                }
+            }
+            if (ans == Int.MAX_VALUE) {
+                ans = 0
+            }
+            return ans
+        }
+
         fun minSubArrayLen(target: Int, nums: IntArray): Int {
             var i = 0
             var j = 0
