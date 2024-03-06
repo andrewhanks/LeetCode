2e4cc8d12f98231cbacff4fc74739f7748c79b7f
Andrew Li <andrewhanks1113@gmail.com>
2024-03-06 10:27:24 +0800

    Add question 541 solution 2

--- src/main/kotlin/questions/Question_541_Reverse_String_II.kt
+++ src/main/kotlin/questions/Question_541_Reverse_String_II.kt
@@ -25,5 +25,34 @@ class Question_541_Reverse_String_II {
             }
             return result.toString()
         }
+
+        fun reverseStrManually(s: String, k: Int): String {
+            val str = s.toCharArray()
+            val n = str.size
+            var i = 0
+            while (i <= n - 1) {
+                if (i + k - 1 <= n - 1) {
+                    reverseK(i, i + k - 1, str)
+                } else {
+                    //for fewer than k characters left (edge case)
+                    reverseK(i, n - 1, str)
+                }
+                i += 2 * k
+            }
+            val ans = String(str)
+            return ans
+        }
+
+        fun reverseK(i: Int, j: Int, str: CharArray) {
+            var i = i
+            var j = j
+            while (i < j) {
+                val temp = str[i]
+                str[i] = str[j]
+                str[j] = temp
+                i++
+                j--
+            }
+        }
     }
 }
