ad49bec9e5346f6db2a4b0be2f57270ca1d5ebcd
Andrew Li <ali@bn.com>
2024-03-22 12:57:45 +0800

    Add question 17 another solution

--- src/main/kotlin/questions/Question_17_Letter_Combinations_of_a_Phone_Number.kt
+++ src/main/kotlin/questions/Question_17_Letter_Combinations_of_a_Phone_Number.kt
@@ -5,14 +5,45 @@ class Question_17_Letter_Combinations_of_a_Phone_Number {
     companion object {
 
         fun runQuestion() {
+//            Input: digits = "23"
+//            Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
             val number = "23"
             val result = letterCombinations(number)
             println("Question 17: $result")
         }
 
-        val resultString = ArrayList<String>()
+        val map = listOf("0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz")
 
         fun letterCombinations(digits: String): List<String> {
+            if (digits.length == 0) {
+                return listOf()
+            }
+            val result: MutableList<String> = mutableListOf()
+            val temp: StringBuilder = StringBuilder("")
+            combination(digits, 0, result, temp)
+            return result
+        }
+
+        fun combination(digits: String, start: Int, result: MutableList<String>, temp: StringBuilder) {
+            if (temp.length == digits.length) {
+                result.add(temp.toString())
+                return
+            }
+            val characters = map[digits[start].digitToInt()]
+            for (count in 0..characters.length - 1) {
+                combine(characters[count], digits, start, result, temp)
+            }
+        }
+
+        fun combine(alphabet: Char, digits: String, start: Int, result: MutableList<String>, temp: StringBuilder) {
+            temp.append(alphabet)
+            combination(digits, start + 1, result, temp)
+            temp.deleteAt(temp.length - 1)
+        }
+
+        val resultString = ArrayList<String>()
+
+        fun letterCombinationsOld(digits: String): List<String> {
 
             if (digits.isEmpty()) {
                 return emptyList()
