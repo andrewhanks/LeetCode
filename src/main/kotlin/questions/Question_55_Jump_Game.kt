e6043c0ae72faa42e0b80d5b7798f285029f0460
Andrew Li <ali@bn.com>
2024-04-01 10:46:45 +0800

    Add question 55 another solution

--- src/main/kotlin/questions/Question_55_Jump_Game.kt
+++ src/main/kotlin/questions/Question_55_Jump_Game.kt
@@ -1,17 +1,31 @@
 package questions
 
+import kotlin.math.max
+
 class Question_55_Jump_Game {
 
     companion object {
 
         fun runQuestion() {
-            // [3,2,1,0,4]
+//            Input: nums = [3,2,1,0,4]
+//            Output: false
             val array = intArrayOf(3, 2, 1, 0, 4)
             val result = canJump(array)
             println("Question 55: $result")
         }
 
         fun canJump(nums: IntArray): Boolean {
+            var max = 0
+            for (count in 0..nums.size - 2) {
+                if (nums[count] == 0 && max <= count) {
+                    return false
+                }
+                max = max(max, nums[count] + count)
+            }
+            return max >= nums.size - 1
+        }
+
+        fun canJumpOld(nums: IntArray): Boolean {
             if (nums.size == 1) {
                 return true
             }
