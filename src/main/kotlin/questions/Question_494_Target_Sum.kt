f0730ef8efe3253f82723703db3eafbaa91e53c7
Andrew Li <ali@bn.com>
2024-04-16 13:10:33 +0800

    Add question 494

---
+++ src/main/kotlin/questions/Question_494_Target_Sum.kt
@@ -0,0 +1,34 @@
+package questions
+
+
+class Question_494_Target_Sum {
+
+    companion object {
+
+        fun runQuestion() {
+//            Input: nums = [1,1,1,1,1], target = 3
+//            Output: 5
+            val nums = intArrayOf(1, 1, 1, 1, 1)
+            val target = 3
+            val result = findTargetSumWays(nums, target)
+            println("Question 494: $result")
+        }
+
+        fun findTargetSumWays(nums: IntArray, target: Int): Int {
+            val sum = nums.sum()
+            val sum2 = (sum - target) / 2
+            val result = Array(nums.size + 1) { Array(sum2 + 1) { 0 } }
+            result[0][0] = 1
+            for (i in 1..result.size - 1) {
+                for (j in 0..result[0].size - 1) {
+                    if (nums[i - 1] <= j) {
+                        result[i][j] = result[i - 1][j - nums[i - 1]] + result[i - 1][j]
+                    } else {
+                        result[i][j] = result[i - 1][j]
+                    }
+                }
+            }
+            return result[result.size - 1][result[0].size - 1]
+        }
+    }
+}
