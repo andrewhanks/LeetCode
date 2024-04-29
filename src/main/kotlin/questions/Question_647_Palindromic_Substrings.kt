7d3d0c10787674a7a731c48485c4f886bbc055b3
Andrew Li <ali@bn.com>
2024-04-29 17:14:33 +0800

    Add question 647 another solution

--- src/main/kotlin/questions/Question_647_Palindromic_Substrings.kt
+++ src/main/kotlin/questions/Question_647_Palindromic_Substrings.kt
@@ -8,11 +8,30 @@ class Question_647_Palindromic_Substrings {
 //            Input: s = "aaa"
 //            Output: 6
             val s = "aaa"
-            val result = countSubstrings(s)
+            val result = countSubstringsBruteForce(s)
             println("Question 647: $result")
         }
 
-        fun countSubstrings(s: String): Int {
+        fun countSubstringsDp(s: String): Int {
+            val result = Array(s.length) { Array<Boolean>(s.length) { false } }
+            var count = 0
+            for (i in 0..s.length - 1) {
+                for (j in 0..i) {
+                    if (s[j] == s[i] && i - j < 2) {
+                        result[j][i] = true
+                        // println("i = $i, j = $j")
+                        count++
+                    } else if (s[j] == s[i] && i - 1 >= 0 && j + 1 <= s.length - 1 && result[j + 1][i - 1]) {
+                        result[j][i] = true
+                        // println("i = $i, j = $j")
+                        count++
+                    }
+                }
+            }
+            return count
+        }
+
+        fun countSubstringsBruteForce(s: String): Int {
             var count = 0
             for (i in 0..s.length - 1) {
                 for (j in 0..i) {
