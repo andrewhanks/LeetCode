27ced1b40a62fd60406a0d7d87bc1a6642278ada
Andrew Li <ali@bn.com>
2024-05-07 14:41:54 +0800

    Add question 130 another solution

--- src/main/kotlin/questions/Question_130_Surrounded_Regions.kt
+++ src/main/kotlin/questions/Question_130_Surrounded_Regions.kt
@@ -8,39 +8,50 @@ class Question_130_Surrounded_Regions {
         fun runQuestion() {
 //            Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
 //            Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
-            val charArray = arrayOf(
+            val board = arrayOf(
                 charArrayOf('X', 'X', 'X', 'X'),
                 charArrayOf('X', 'O', 'O', 'X'),
                 charArrayOf('X', 'X', 'O', 'X'),
                 charArrayOf('X', 'O', 'X', 'X')
             )
-            solve(charArray)
-            var result = "["
-            charArray.forEachIndexed { firstIndex, firstI ->
-                charArray[firstIndex].forEachIndexed { secondIndex, secondI ->
-                    if (0 == secondIndex) {
-                        result += "["
-                    }
-                    result += secondI
-                    if (secondIndex == charArray[firstIndex].size - 1) {
-                        result += "]"
-                    } else {
-                        result += ","
-                    }
-                }
-                if (firstIndex == charArray.size - 1) {
-                    result += "]"
-                } else {
-                    result += ","
-                }
-            }
-            if (']' != result.last()) {
-                result += "]"
-            }
-            println("Question 130: $result")
+            solve(board)
+            println("Question 130: ${board.contentDeepToString()}")
         }
 
         fun solve(board: Array<CharArray>): Unit {
+            for (i in 0..board.size - 1) {
+                for (j in 0..board[0].size - 1) {
+                    if (i == 0 || i == board.size - 1 || j == 0 || j == board[0].size - 1) {
+                        check(board, i, j)
+                    }
+                }
+            }
+            for (i in 0..board.size - 1) {
+                for (j in 0..board[0].size - 1) {
+                    if (board[i][j] != 'T') {
+                        board[i][j] = 'X'
+                    } else {
+                        board[i][j] = 'O'
+                    }
+                }
+            }
+        }
+
+        fun check(board: Array<CharArray>, i: Int, j: Int) {
+            if (i < 0 || i > board.size - 1 || j < 0 || j > board[0].size - 1) {
+                return
+            }
+            if (board[i][j] != 'O') {
+                return
+            }
+            board[i][j] = 'T'
+            check(board, i - 1, j)
+            check(board, i + 1, j)
+            check(board, i, j - 1)
+            check(board, i, j + 1)
+        }
+
+        fun solveOldSolution(board: Array<CharArray>): Unit {
             for (i in 0..board.size - 1) {
                 if (board[i][0] == 'O') {
                     dfs(board, i, 0)
