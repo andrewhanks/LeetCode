007a9871748bd8d17b3143f8005febd9e6782fd4
Andrew Li <ali@bn.com>
2024-05-13 12:43:15 +0800

    Add question 309 another solution

--- src/main/kotlin/questions/Question_309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.kt
+++ src/main/kotlin/questions/Question_309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.kt
@@ -15,6 +15,24 @@ class Question_309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown {
         }
 
         fun maxProfit(prices: IntArray): Int {
+            val empty = IntArray(prices.size) { 0 }
+            val hold = IntArray(prices.size) { 0 }
+            val cooldown = IntArray(prices.size) { 0 }
+            empty[0] = 0
+            hold[0] = -prices[0]
+            cooldown[0] = 0
+            for (count in 1..prices.size - 1) {
+                empty[count] = max(empty[count - 1], cooldown[count - 1])
+                hold[count] = max(hold[count - 1], empty[count - 1] - prices[count])
+                cooldown[count] = hold[count - 1] + prices[count]
+            }
+            // println(empty.contentToString())
+            // println(hold.contentToString())
+            // println(cooldown.contentToString())
+            return max(empty[prices.size - 1], cooldown[prices.size - 1])
+        }
+
+        fun maxProfitDp(prices: IntArray): Int {
             if (prices.size <= 1) {
                 return 0
             }
