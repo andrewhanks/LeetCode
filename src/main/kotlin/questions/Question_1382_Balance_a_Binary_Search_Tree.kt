eb8aef1ae8ef41f6e93d5cd0721163e3b9480fc7
Andrew Li <ali@bn.com>
2024-05-14 15:17:36 +0800

    Add question 1382

---
+++ src/main/kotlin/questions/Question_1382_Balance_a_Binary_Search_Tree.kt
@@ -0,0 +1,65 @@
+package questions
+
+import models.TreeNode
+
+
+class Question_1382_Balance_a_Binary_Search_Tree {
+
+    companion object {
+
+        fun runQuestion() {
+//            Input: root = [1,null,2,null,3,null,4,null,null]
+//            Output: [2,1,3,null,null,null,4]
+            val root: TreeNode
+            val treeNode1 = TreeNode(1)
+            val treeNode2 = TreeNode(2)
+            val treeNode3 = TreeNode(3)
+            val treeNode4 = TreeNode(4)
+            treeNode1.right = treeNode2
+            treeNode2.right = treeNode3
+            treeNode3.right = treeNode4
+            root = treeNode1
+            val treeNode = balanceBST(root)
+            var result = "["
+            result = Utils.printTreeNodeByLevelOrder(result, treeNode)
+            result += "]"
+            println("Question 1382: $result")
+        }
+
+        fun balanceBST(root: TreeNode?): TreeNode? {
+            val inorder: MutableList<TreeNode> = mutableListOf()
+            inorder(root, inorder)
+            return buildBst(inorder, 0, inorder.size - 1)
+        }
+
+        fun inorder(root: TreeNode?, inorder: MutableList<TreeNode>) {
+            if (root == null) {
+                return
+            }
+            if (root?.left != null) {
+                inorder(root?.left, inorder)
+            }
+            inorder.add(root)
+            if (root?.right != null) {
+                inorder(root?.right, inorder)
+            }
+        }
+
+        fun buildBst(inorder: MutableList<TreeNode>, start: Int, end: Int): TreeNode? {
+            // println("start = $start, end = $end")
+            if (start > end) {
+                return null
+            }
+            if (start == end) {
+                inorder[start]?.left = null
+                inorder[start]?.right = null
+                return inorder[start]
+            }
+            val index = (start + end) / 2
+            // println("index = $index, inorder[index]?.`val` = ${inorder[index]?.`val`}")
+            inorder[index]?.left = buildBst(inorder, start, index - 1)
+            inorder[index]?.right = buildBst(inorder, index + 1, end)
+            return inorder[index]
+        }
+    }
+}
