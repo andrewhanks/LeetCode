20a921e68f119bebdb99946fe723a9fdd5c3aa08
Andrew Li <ali@bn.com>
2024-05-21 13:55:15 +0800

    Add question 189 another solution

--- src/main/kotlin/questions/Question_189_Rotate_Array.kt
+++ src/main/kotlin/questions/Question_189_Rotate_Array.kt
@@ -5,23 +5,40 @@ class Question_189_Rotate_Array {
     companion object {
 
         fun runQuestion() {
-            // [1,2,3,4,5,6,7]
+//            Input: nums = [1,2,3,4,5,6,7], k = 3
+//            Output: [5,6,7,1,2,3,4]
             val k = 3
-            val numberArray = intArrayOf(1, 2, 3, 4, 5, 6, 7)
-            rotate(numberArray, k)
-            var result = "["
-            numberArray.forEachIndexed { index, i ->
-                result += i
-                if (index == numberArray.size - 1) {
-                    result += "]"
-                } else {
-                    result += ","
-                }
-            }
-            println("Question 75: $result")
+            val nums = intArrayOf(1, 2, 3, 4, 5, 6, 7)
+            rotate(nums, k)
+            println("Question 189: ${nums.contentToString()}")
         }
 
         fun rotate(nums: IntArray, k: Int): Unit {
+            if (nums.size == 1) {
+                return
+            }
+            var tempK = k
+            while (tempK >= nums.size) {
+                tempK -= nums.size
+            }
+            rotate(nums, 0, nums.size - tempK - 1)
+            rotate(nums, nums.size - tempK, nums.size - 1)
+            rotate(nums, 0, nums.size - 1)
+        }
+
+        fun rotate(nums: IntArray, start: Int, end: Int) {
+            var i = start
+            var j = end
+            while (i < j) {
+                val temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+                i++
+                j--
+            }
+        }
+
+        fun rotateOldSolution(nums: IntArray, k: Int): Unit {
             var tempK = k
             while (tempK >= nums.size) {
                 tempK -= nums.size
