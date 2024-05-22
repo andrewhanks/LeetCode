8d49d14d16678da6524144e0cb40a67375fab23f
Andrew Li <ali@bn.com>
2024-05-22 10:38:52 +0800

    Add question 925

---
+++ src/main/kotlin/questions/Question_925_Long_Pressed_Name.kt
@@ -0,0 +1,49 @@
+package questions
+
+class Question_925_Long_Pressed_Name {
+
+    companion object {
+
+        fun runQuestion() {
+//            Input: name = "saeed", typed = "ssaaedd"
+//            Output: false
+            val name = "saeed"
+            val typed = "ssaaedd"
+            val result = isLongPressedName(name, typed)
+            println("Question 925: $result")
+        }
+
+        fun isLongPressedName(name: String, typed: String): Boolean {
+            val nameList: MutableList<Char> = mutableListOf()
+            val nameNum: MutableList<Int> = mutableListOf()
+            val typedList: MutableList<Char> = mutableListOf()
+            val typedNum: MutableList<Int> = mutableListOf()
+            for (count in 0..name.length - 1) {
+                if (count == 0 || (count > 0 && name[count - 1] != name[count])) {
+                    nameNum.add(1)
+                    nameList.add(name[count])
+                } else {
+                    nameNum[nameNum.size - 1] = nameNum[nameNum.size - 1]!! + 1
+                }
+            }
+            for (count in 0..typed.length - 1) {
+                if (count == 0 || (count > 0 && typed[count - 1] != typed[count])) {
+                    typedNum.add(1)
+                    typedList.add(typed[count])
+                } else {
+                    typedNum[typedNum.size - 1] = typedNum[typedNum.size - 1]!! + 1
+                }
+            }
+            // println("nameList.size = ${nameList.size}, typedList.size = ${typedList.size}")
+            if (nameList.size != typedList.size) {
+                return false
+            }
+            for (count in 0..nameList.size - 1) {
+                if (nameList[count] != typedList[count] || nameNum[count] > typedNum[count]) {
+                    return false
+                }
+            }
+            return true
+        }
+    }
+}
