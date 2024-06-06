90d61bcc360a21df780dbea44e02d02b708a10ef
Andrew Li <ali@bn.com>
2024-06-06 10:31:02 +0800

    Add question 75 another solution

--- src/main/kotlin/questions/Question_75_Sort_Colors.kt
+++ src/main/kotlin/questions/Question_75_Sort_Colors.kt
@@ -5,7 +5,8 @@ class Question_75_Sort_Colors {
     companion object {
 
         fun runQuestion() {
-            // [2,0,2,1,1,0]
+//            Input: nums = [2,0,2,1,1,0]
+//            Output: [0,0,1,1,2,2]
             val numberArray = intArrayOf(2, 0, 2, 1, 1, 0)
             sortColors(numberArray)
             var result = "["
@@ -27,6 +28,25 @@ class Question_75_Sort_Colors {
                         val tempNum = nums[y]
                         nums[y] = nums[y + 1]
                         nums[y + 1] = tempNum
+                    }
+                }
+            }
+        }
+
+        fun sortColorsSelectionSort(nums: IntArray): Unit {
+            var map: MutableMap<Int, Int> = mutableMapOf(0 to 0)
+            for (i in 0..nums.size - 2) {
+                var min = nums[i + 1]
+                map[min] = i + 1
+                for (j in i + 1..nums.size - 1) {
+                    if (min > nums[j]) {
+                        min = nums[j]
+                        map[min] = j
+                    }
+                    if (j == nums.size - 1 && nums[map[min]!!] < nums[i]) {
+                        val temp = nums[map[min]!!]
+                        nums[map[min]!!] = nums[i]
+                        nums[i] = temp
                     }
                 }
             }
