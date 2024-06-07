5d94f48b7c5e7edec5e9c281f3343721b42391f4
Andrew Li <ali@bn.com>
2024-06-07 13:55:11 +0800

    Add question 139 another solution

--- src/main/kotlin/questions/Question_139_Word_Break.kt
+++ src/main/kotlin/questions/Question_139_Word_Break.kt
@@ -5,6 +5,8 @@ class Question_139_Word_Break {
     companion object {
 
         fun runQuestion() {
+//            Input: s = "leetcode", wordDict = ["leet","code"]
+//            Output: true
             val inputString = "leetcode"
             val inputWordList = mutableListOf<String>()
             inputWordList.add("leet")
@@ -13,9 +15,28 @@ class Question_139_Word_Break {
             println("Question 139: $result")
         }
 
+        fun wordBreak(s: String, wordDict: List<String>): Boolean {
+            val result = Array(s.length + 1) { false }
+            result[0] = true
+            for (count in 0..s.length - 1) {
+                for (number in 0..wordDict.size - 1) {
+                    if (wordDict[number].length <= count + 1) {
+                        val postStringStartIndex = count - wordDict[number].length + 1
+                        val postString = s.substring(postStringStartIndex..count)
+                        // println("count = $count, postStringStartIndex = $postStringStartIndex, postString = $postString, wordDict.contains(postString) = ${wordDict.contains(postString)}")
+                        if (result[postStringStartIndex] && wordDict.contains(postString)) {
+                            result[count + 1] = true
+                        }
+                    }
+                }
+            }
+            // println("result = ${result.contentToString()}")
+            return result[result.size - 1]
+        }
+
         var map: MutableMap<String, Boolean> = HashMap()
 
-        fun wordBreak(s: String, wordDict: List<String>): Boolean {
+        fun wordBreakWithMemorization(s: String, wordDict: List<String>): Boolean {
             if (wordDict.contains(s)) {
                 return true
             }
