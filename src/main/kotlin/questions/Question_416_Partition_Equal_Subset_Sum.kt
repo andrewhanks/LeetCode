b984aa2db7c6ff636907c2c04674eab1ebee01d7
Andrew Li <ali@bn.com>
2024-06-14 14:05:21 +0800

    Add question 416 another solution

--- src/main/kotlin/questions/Question_416_Partition_Equal_Subset_Sum.kt
+++ src/main/kotlin/questions/Question_416_Partition_Equal_Subset_Sum.kt
@@ -13,6 +13,37 @@ class Question_416_Partition_Equal_Subset_Sum {
         }
 
         fun canPartition(nums: IntArray): Boolean {
+            val sum = nums.sum()
+            if (sum % 2 == 1) {
+                return false
+            }
+            val result: Array<Array<Boolean?>> = Array(nums.size + 1) { Array(sum / 2 + 1) { null } }
+            val finalResult = dp(nums, result, result.size - 1, result[0].size - 1)
+            // println("result = ${result.contentDeepToString()}")
+            return finalResult
+        }
+
+        fun dp(nums: IntArray, result: Array<Array<Boolean?>>, i: Int, j: Int): Boolean {
+            if (j == 0) {
+                result[i][j] = true
+                return true
+            }
+            if (i == 0) {
+                result[i][j] = false
+                return false
+            }
+            if (result[i][j] != null) {
+                return result[i][j]!!
+            }
+            if (nums[i - 1] <= j) {
+                result[i][j] = dp(nums, result, i - 1, j - nums[i - 1]) || dp(nums, result, i - 1, j)
+            } else {
+                result[i][j] = dp(nums, result, i - 1, j)
+            }
+            return result[i][j]!!
+        }
+
+        fun canPartitionBottomUp(nums: IntArray): Boolean {
             val total = nums.sum()
             if (total % 2 != 0) {
                 return false
