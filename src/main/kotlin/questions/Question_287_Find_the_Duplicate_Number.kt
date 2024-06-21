9ddd54798c31ada812909fe8db9cd8cd7a7d6ea5
Andrew Li <ali@bn.com>
2024-06-21 13:18:38 +0800

    Add question 287 another solution

--- src/main/kotlin/questions/Question_287_Find_the_Duplicate_Number.kt
+++ src/main/kotlin/questions/Question_287_Find_the_Duplicate_Number.kt
@@ -5,12 +5,25 @@ class Question_287_Find_the_Duplicate_Number {
     companion object {
 
         fun runQuestion() {
+//            Input: nums = [1,3,4,2,2]
+//            Output: 2
             val inputIntArray = intArrayOf(1, 3, 4, 2, 2)
             val result = findDuplicate(inputIntArray)
             println("Question 287: $result")
         }
 
         fun findDuplicate(nums: IntArray): Int {
+            for (i in 0..nums.size - 2) {
+                for (j in i + 1..nums.size - 1) {
+                    if (nums[i] == nums[j]) {
+                        return nums[i]
+                    }
+                }
+            }
+            return 0
+        }
+
+        fun findDuplicateOldSolution(nums: IntArray): Int {
             var head = 1
             var tail = nums.size - 1
             while (head < tail) {
