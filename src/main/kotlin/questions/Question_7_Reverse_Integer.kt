efa956d52be5a982577b650089fa67a9bf34fceb
Andrew Li <ali@bn.com>
2024-07-03 14:06:22 +0800

    Add question 7 another solution

--- src/main/kotlin/questions/Question_7_Reverse_Integer.kt
+++ src/main/kotlin/questions/Question_7_Reverse_Integer.kt
@@ -13,6 +13,21 @@ class Question_7_Reverse_Integer {
         }
 
         fun reverse(x: Int): Int {
+            var reversedLong = 0L
+            var tempX = x
+            while (tempX != 0) {
+                val remain = tempX % 10
+                reversedLong = reversedLong * 10 + remain
+                tempX = tempX / 10
+            }
+            if (reversedLong < Int.MIN_VALUE || reversedLong > Int.MAX_VALUE) {
+                return 0
+            } else {
+                return reversedLong.toInt()
+            }
+        }
+
+        fun reverseAnotherSolution(x: Int): Int {
             var stringX = x.toString()
             val smallThanZero = stringX[0] == '-'
             val reversedLong = if (smallThanZero) {
