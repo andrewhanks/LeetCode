f87b331edc1ecb43e06769d1bf83eb73e01d909e
Andrew Li <ali@bn.com>
2024-07-09 13:44:57 +0800

    Add question 16 another solution

--- src/main/kotlin/questions/Question_16_3Sum_Closest.kt
+++ src/main/kotlin/questions/Question_16_3Sum_Closest.kt
@@ -1,17 +1,48 @@
 package questions
 
+import kotlin.math.abs
+
 class Question_16_3Sum_Closest {
 
     companion object {
 
         fun runQuestion() {
-            val inputNums = intArrayOf(1, 1, -1, -1, 3)
-            val inputTarget = -1
+//            Input: nums = [-1,2,1,-4], target = 1
+//            Output: 2
+            val inputNums = intArrayOf(-1, 2, 1, -4)
+            val inputTarget = 1
             val result = threeSumClosest(inputNums, inputTarget)
             println("Question 16: $result")
         }
 
         fun threeSumClosest(nums: IntArray, target: Int): Int {
+            nums.sort()
+            var minDiff = Int.MAX_VALUE
+            var ans = 0
+            for (i in 0..nums.size - 3) {
+                var j = i + 1
+                var k = nums.size - 1
+                while (j < k) {
+                    val sum = nums[i] + nums[j] + nums[k]
+                    val diff = sum - target
+                    if (abs(diff) < minDiff) {
+                        ans = sum
+                        minDiff = abs(diff)
+                    }
+                    // println("nums[$i] = ${nums[i]}, nums[$j] = ${nums[j]}, nums[$k] = ${nums[k]}, minDiff = $minDiff")
+                    if (diff > 0) {
+                        k--
+                    } else if (diff < 0) {
+                        j++
+                    } else {
+                        return target
+                    }
+                }
+            }
+            return ans
+        }
+
+        fun threeSumClosestOldSolution(nums: IntArray, target: Int): Int {
             var result = Int.MAX_VALUE
             for (i in 0..nums.size - 1) {
                 for (j in i + 1..nums.size - 1) {
