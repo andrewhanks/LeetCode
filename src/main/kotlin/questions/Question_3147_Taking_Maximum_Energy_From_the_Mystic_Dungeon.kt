e5310af1ba2b1ffc2e36dc9b0e9745b1ef6ead69
Andrew Li <ali@bn.com>
2024-07-11 15:31:26 +0800

    Add question 3147 another solution

--- src/main/kotlin/questions/Question_3147_Taking_Maximum_Energy_From_the_Mystic_Dungeon.kt
+++ src/main/kotlin/questions/Question_3147_Taking_Maximum_Energy_From_the_Mystic_Dungeon.kt
@@ -1,5 +1,7 @@
 package questions
 
+import kotlin.math.max
+
 
 class Question_3147_Taking_Maximum_Energy_From_the_Mystic_Dungeon {
 
@@ -12,6 +14,18 @@ class Question_3147_Taking_Maximum_Energy_From_the_Mystic_Dungeon {
             val k = 3
             val result = maximumEnergy(energy, k)
             println("Question 3147: $result")
+        }
+
+        fun maximumEnergyQuicker(energy: IntArray, k: Int): Int {
+            var result = Int.MIN_VALUE
+            for (i in 1..k) {
+                var prefixSum = 0
+                for (j in energy.size - i downTo 0 step k) {
+                    prefixSum += energy[j]
+                    result = max(result, prefixSum)
+                }
+            }
+            return result
         }
 
         fun maximumEnergy(energy: IntArray, k: Int): Int {
