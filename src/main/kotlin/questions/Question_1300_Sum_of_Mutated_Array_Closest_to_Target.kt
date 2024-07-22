5ac98f5ed8f58b5d07778e1c239716ab46e741b3
Andrew Li <ali@bn.com>
2024-07-22 14:02:10 +0800

    Add question 1300 web solution

--- src/main/kotlin/questions/Question_1300_Sum_of_Mutated_Array_Closest_to_Target.kt
+++ src/main/kotlin/questions/Question_1300_Sum_of_Mutated_Array_Closest_to_Target.kt
@@ -1,5 +1,8 @@
 package questions
 
+import kotlin.math.abs
+import kotlin.math.min
+
 class Question_1300_Sum_of_Mutated_Array_Closest_to_Target {
 
     companion object {
@@ -47,6 +50,47 @@ class Question_1300_Sum_of_Mutated_Array_Closest_to_Target {
                 ans = arr[arr.size - 1]
             }
             return ans
+        }
+
+        fun findBestValueWebBruteForce(arr: IntArray, target: Int): Int {
+            arr.sort()
+            val size = arr.size
+            var index = 0
+            var tempTarget = target
+            while (index < size && tempTarget > arr[index] * (size - index)) {
+                tempTarget -= arr[index]
+                index++
+            }
+            if (index == size) {
+                return arr[size - 1]
+            }
+            var ans = tempTarget / (size - index)
+            if (tempTarget - ans * (size - index) > (ans + 1) * (size - index) - tempTarget) {
+                ans++
+            }
+            return ans
+        }
+
+        fun findBestValueWebBinarySearch(arr: IntArray, target: Int): Int {
+            var left = 0
+            var right = target
+            while (left < right) {
+                val mid = left + (right - left) / 2
+                if (diff(arr, mid, target) > diff(arr, mid + 1, target)) {
+                    left = mid + 1
+                } else {
+                    right = mid
+                }
+            }
+            return left
+        }
+
+        fun diff(arr: IntArray, min: Int, target: Int): Int {
+            var diff = 0
+            for (count in 0..arr.size - 1) {
+                diff += min(min, arr[count])
+            }
+            return abs(target - diff)
         }
     }
 }
