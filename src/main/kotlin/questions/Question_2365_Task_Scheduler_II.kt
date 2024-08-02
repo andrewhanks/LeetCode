06f50fcdd246ba95f7443f6bdde47919106aca10
Andrew Li <ali@bn.com>
2024-08-02 16:12:57 +0800

    Add question 2365

---
+++ src/main/kotlin/questions/Question_2365_Task_Scheduler_II.kt
@@ -0,0 +1,30 @@
+package questions
+
+import kotlin.math.max
+
+
+class Question_2365_Task_Scheduler_II {
+
+    companion object {
+
+        fun runQuestion() {
+//            Input: tasks = [1,2,1,2,3,1], space = 3
+//            Output: 9
+            val tasks = intArrayOf(1, 2, 1, 2, 3, 1)
+            val space = 3
+            val result = taskSchedulerII(tasks, space)
+            println("Question 2488: $result")
+        }
+
+        fun taskSchedulerII(tasks: IntArray, space: Int): Long {
+            val map: MutableMap<Int, Long> = mutableMapOf()
+            var ans = 0L
+            for (count in 0..tasks.size - 1) {
+                ans++
+                ans = max(ans, map.getOrDefault(tasks[count], 0))
+                map[tasks[count]] = ans + space + 1
+            }
+            return ans
+        }
+    }
+}
