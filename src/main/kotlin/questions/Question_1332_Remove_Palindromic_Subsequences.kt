b7120c925b6d1e53793f077ac9cfb0880c5c506e
Andrew Li <ali@bn.com>
2024-08-08 14:45:32 +0800

    Add question 1332

---
+++ src/main/kotlin/questions/Question_1332_Remove_Palindromic_Subsequences.kt
@@ -0,0 +1,48 @@
+package questions
+
+class Question_1332_Remove_Palindromic_Subsequences {
+
+    companion object {
+
+        fun runQuestion() {
+//            Input: s = "ababa"
+//            Output: 1
+            val inputString = "ababa"
+            val result = removePalindromeSub(inputString)
+            println("Question 1332: $result")
+        }
+
+        fun removePalindromeSub(s: String): Int {
+            if (isPalidrome(s)) {
+                return 1
+            }
+            var numA = 0
+            var numB = 0
+            for (count in 0..s.length - 1) {
+                if (s[count] == 'a') {
+                    numA++
+                } else {
+                    numB++
+                }
+            }
+            if (numA != 0 && numB != 0) {
+                return 2
+            } else {
+                return 1
+            }
+        }
+
+        fun isPalidrome(s: String): Boolean {
+            var i = 0
+            var j = s.length - 1
+            while (i < j) {
+                if (s[i] != s[j]) {
+                    return false
+                }
+                i++
+                j--
+            }
+            return true
+        }
+    }
+}
