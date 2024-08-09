a3f824471f918ddc4a18193f5117cf38d1bacc74
Andrew Li <ali@bn.com>
2024-08-09 11:18:31 +0800

    Add question 1540

---
+++ src/main/kotlin/questions/Question_1540_Can_Convert_String_in_K_Moves.kt
@@ -0,0 +1,45 @@
+package questions
+
+class Question_1540_Can_Convert_String_in_K_Moves {
+
+    companion object {
+
+        fun runQuestion() {
+//            Input: s = "aab", t = "bbb", k = 27
+//            Output: true
+            val s = "aab"
+            val t = "bbb"
+            val k = 27
+            val result = canConvertString(s, t, k)
+            println("Question 1540: $result")
+        }
+
+        fun canConvertString(s: String, t: String, k: Int): Boolean {
+            if (s.length != t.length) {
+                return false
+            }
+            val diff = IntArray(s.length) { 0 }
+            for (count in 0..diff.size - 1) {
+                diff[count] = if (t[count] >= s[count]) {
+                    t[count] - s[count]
+                } else {
+                    t[count] - s[count] + 26
+                }
+            }
+            val used = IntArray(27) { 0 }
+            for (count in 0..diff.size - 1) {
+                if (diff[count] == 0) {
+                    continue
+                }
+                var tempDiff = diff[count]
+                // println("$tempDiff+26*used[$tempDiff] = ${tempDiff+26*used[tempDiff]}")
+                if (tempDiff + 26 * used[tempDiff] <= k) {
+                    used[tempDiff]++
+                } else {
+                    return false
+                }
+            }
+            return true
+        }
+    }
+}
