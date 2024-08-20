b9ae0032f2171e4ab8fb2b1e3d7c6f993ce38d26
Andrew Li <ali@bn.com>
2024-08-20 18:15:26 +0800

    Add question 838

---
+++ src/main/kotlin/questions/Question_838_Push_Dominoes.kt
@@ -0,0 +1,86 @@
+package questions
+
+import java.util.*
+
+class Question_838_Push_Dominoes {
+
+    companion object {
+
+        fun runQuestion() {
+//            Input: dominoes = ".L.R...LR..L.."
+//            Output: "LL.RR.LLRRLL.."
+            val dominoes = ".L.R...LR..L.."
+            val result = pushDominoes(dominoes)
+            println("Question 838: $result")
+        }
+
+        fun pushDominoes(dominoes: String): String {
+            val queue: Queue<Char> = LinkedList()
+            val result = StringBuilder()
+            val temp: MutableList<Char> = mutableListOf()
+            for (count in 0..dominoes.length - 1) {
+                val item = dominoes[count]
+                queue.add(item)
+                if (item == 'L' || count == dominoes.length - 1) {
+                    while (!queue.isEmpty()) {
+                        temp.add(queue.remove())
+                    }
+                    var left = temp.lastIndexOf('R')
+                    var right = temp.indexOf('L')
+                    if (left == -1 && right != -1) {
+                        var hasLeft = false
+                        for (count in temp.size - 1 downTo 0) {
+                            if (hasLeft) {
+                                temp[count] = 'L'
+                            }
+                            if (temp[count] == 'L') {
+                                hasLeft = true
+                            }
+                        }
+                        appendToResult(result, temp)
+                        continue
+                    } else if (right == -1 && left != -1) {
+                        var hasRight = false
+                        for (count in 0..temp.size - 1) {
+                            if (hasRight) {
+                                temp[count] = 'R'
+                            }
+                            if (temp[count] == 'R') {
+                                hasRight = true
+                            }
+                        }
+                        appendToResult(result, temp)
+                        continue
+                    } else if (right == -1 && left == -1) {
+                        appendToResult(result, temp)
+                        continue
+                    } else {
+                        var firstLeft = temp.indexOf('R')
+                        var lastRight = temp.lastIndexOf('L')
+                        for (index in firstLeft..left) {
+                            temp[index] = 'R'
+                        }
+                        for (index in right..lastRight) {
+                            temp[index] = 'L'
+                        }
+                        while (left < right) {
+                            temp[left] = 'R'
+                            temp[right] = 'L'
+                            left++
+                            right--
+                        }
+                        appendToResult(result, temp)
+                    }
+                }
+            }
+            return result.toString()
+        }
+
+        fun appendToResult(result: StringBuilder, temp: MutableList<Char>) {
+            for (item in temp) {
+                result.append(item)
+            }
+            temp.clear()
+        }
+    }
+}
