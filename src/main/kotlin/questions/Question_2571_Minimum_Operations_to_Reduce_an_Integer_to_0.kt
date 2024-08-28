89de874d9f37ad82621dd76676ebf16429de1555
Andrew Li <ali@bn.com>
2024-08-28 10:47:32 +0800

    Add question 2571 another solution

--- src/main/kotlin/questions/Question_2571_Minimum_Operations_to_Reduce_an_Integer_to_0.kt
+++ src/main/kotlin/questions/Question_2571_Minimum_Operations_to_Reduce_an_Integer_to_0.kt
@@ -1,6 +1,7 @@
 package questions
 
 import kotlin.math.abs
+import kotlin.math.min
 import kotlin.math.pow
 
 class Question_2571_Minimum_Operations_to_Reduce_an_Integer_to_0 {
@@ -16,6 +17,25 @@ class Question_2571_Minimum_Operations_to_Reduce_an_Integer_to_0 {
         }
 
         fun minOperations(n: Int): Int {
+            return calculate(n)
+        }
+
+        fun calculate(n: Int): Int {
+            var preValue = 1
+            var curValue = 1
+            while (curValue <= n) {
+                preValue = curValue
+                curValue *= 2
+            }
+            val minDiff = min(n - preValue, curValue - n)
+            if (minDiff == 0) {
+                return 1
+            } else {
+                return calculate(minDiff) + 1
+            }
+        }
+
+        fun minOperationsIterative(n: Int): Int {
             var result = n
             var operation = 0
             while (result != 0) {
