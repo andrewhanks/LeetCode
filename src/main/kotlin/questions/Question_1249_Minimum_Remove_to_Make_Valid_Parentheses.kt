7b786f6df5e7d65fc1a7b9cabe088ccbc5f2b15c
Andrew Li <ali@bn.com>
2024-09-03 17:52:32 +0800

    Add question 1249

---
+++ src/main/kotlin/questions/Question_1249_Minimum_Remove_to_Make_Valid_Parentheses.kt
@@ -0,0 +1,51 @@
+package questions
+
+class Question_1249_Minimum_Remove_to_Make_Valid_Parentheses {
+
+    companion object {
+
+        fun runQuestion() {
+//            Input: s = "lee(t(c)o)de)"
+//            Output: "lee(t(c)o)de"
+            val s = "lee(t(c)o)de)"
+            val result = minRemoveToMakeValid(s)
+            println("Question 1249: $result")
+        }
+
+        fun minRemoveToMakeValid(s: String): String {
+            val temp = StringBuilder(s)
+            var left = 0
+            var right = 0
+            var count = 0
+            while (count <= temp.length - 1) {
+                if (temp[count] == '(') {
+                    left++
+                } else if (temp[count] == ')') {
+                    right++
+                }
+                if (right > left) {
+                    temp.deleteAt(count)
+                    right--
+                    count--
+                }
+                count++
+            }
+            left = 0
+            right = 0
+            count = temp.length - 1
+            while (count >= 0) {
+                if (temp[count] == '(') {
+                    left++
+                } else if (temp[count] == ')') {
+                    right++
+                }
+                if (left > right) {
+                    temp.deleteAt(count)
+                    left--
+                }
+                count--
+            }
+            return temp.toString()
+        }
+    }
+}
