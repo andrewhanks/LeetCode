d2a6e09c3b3d99f2ca2389013bd3135e789bb634
Andrew Li <ali@bn.com>
2024-09-10 15:41:47 +0800

    Add question 2304 another solution

--- src/main/kotlin/questions/Question_2304_Minimum_Path_Cost_in_a_Grid.kt
+++ src/main/kotlin/questions/Question_2304_Minimum_Path_Cost_in_a_Grid.kt
@@ -24,6 +24,31 @@ class Question_2304_Minimum_Path_Cost_in_a_Grid {
         }
 
         fun minPathCost(grid: Array<IntArray>, moveCost: Array<IntArray>): Int {
+            val dp = Array(grid.size) { IntArray(grid[0].size) { -1 } }
+            for (count in 0..grid[0].size - 1) {
+                dp[0][count] = grid[0][count]
+            }
+            dp(grid, moveCost, dp, 1)
+            // println("dp = ${dp.contentDeepToString()}")
+            return dp[dp.size - 1].min()
+        }
+
+        fun dp(grid: Array<IntArray>, moveCost: Array<IntArray>, dp: Array<IntArray>, x: Int) {
+            if (x > grid.size - 1) {
+                return
+            }
+            for (i in 0..grid[x].size - 1) {
+                var min = Int.MAX_VALUE
+                for (j in 0..grid[x - 1].size - 1) {
+                    val node = grid[x - 1][j]
+                    min = min(min, dp[x - 1][j] + moveCost[node][i])
+                }
+                dp[x][i] = min + grid[x][i]
+            }
+            dp(grid, moveCost, dp, x + 1)
+        }
+
+        fun minPathCostWithIterative(grid: Array<IntArray>, moveCost: Array<IntArray>): Int {
             var result = IntArray(grid[0].size) { 0 }
             for (i in 0..grid.size - 2) {
                 val temp = MutableList<Int>(grid[0].size) { -1 }
