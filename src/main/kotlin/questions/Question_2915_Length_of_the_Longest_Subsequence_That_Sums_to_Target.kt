85c25d50a150b2ce2d0d1a385e920b7b391868d1
Andrew Li <ali@bn.com>
2024-09-16 10:42:27 +0800

    Add question 2915 another solution

--- src/main/kotlin/questions/Question_2915_Length_of_the_Longest_Subsequence_That_Sums_to_Target.kt
+++ src/main/kotlin/questions/Question_2915_Length_of_the_Longest_Subsequence_That_Sums_to_Target.kt
@@ -35,5 +35,31 @@ class Question_2915_Length_of_the_Longest_Subsequence_That_Sums_to_Target {
             // println("result = ${result.contentDeepToString()}")
             return result[result.size - 1][result[0].size - 1]
         }
+
+        fun lengthOfLongestSubsequenceWithRecursive(nums: List<Int>, target: Int): Int {
+            val result = Array(nums.size + 1) { IntArray(target + 1) { -1 } }
+            dp(nums, result, result.size - 1, result[0].size - 1)
+            var ans = result[result.size - 1][result[0].size - 1]
+            if (ans < 0) {
+                ans = -1
+            }
+            return ans
+        }
+
+        fun dp(nums: List<Int>, result: Array<IntArray>, x: Int, y: Int): Int {
+            if (y == 0) {
+                return 0
+            }
+            if (y < 0 || x < 1) {
+                return Int.MIN_VALUE
+            }
+            if (result[x][y] != -1) {
+                return result[x][y]
+            }
+            val notPick = dp(nums, result, x - 1, y)
+            var pick = dp(nums, result, x - 1, y - nums[x - 1]) + 1
+            result[x][y] = max(notPick, pick)
+            return result[x][y]
+        }
     }
 }
