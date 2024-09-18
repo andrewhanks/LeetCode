f85021bfcdd38b794c97869b52c0b8179928bd53
Andrew Li <ali@bn.com>
2024-09-18 15:09:20 +0800

    Add question 1219 another solution

--- src/main/kotlin/questions/Question_1219_Path_with_Maximum_Gold.kt
+++ src/main/kotlin/questions/Question_1219_Path_with_Maximum_Gold.kt
@@ -52,5 +52,45 @@ class Question_1219_Path_with_Maximum_Gold {
             visited[x][y] = false
             return max + grid[x][y]
         }
+
+        fun getMaximumGoldWithSumParameter(grid: Array<IntArray>): Int {
+            var ans = 0
+            for (x in 0..grid.size - 1) {
+                for (y in 0..grid[0].size - 1) {
+                    if (grid[x][y] == 0) {
+                        continue
+                    }
+                    val visited = Array(grid.size) { Array(grid[0].size) { false } }
+                    ans = max(ans, calculateWithSumParameter(grid, visited, x, y, 0))
+                }
+            }
+            return ans
+        }
+
+        fun calculateWithSumParameter(
+            grid: Array<IntArray>,
+            visited: Array<Array<Boolean>>,
+            x: Int,
+            y: Int,
+            sum: Int
+        ): Int {
+            val dirs = arrayOf(intArrayOf(1, 0), intArrayOf(-1, 0), intArrayOf(0, 1), intArrayOf(0, -1))
+            if (x < 0 || x > grid.size - 1 || y < 0 || y > grid[0].size - 1) {
+                return sum
+            }
+            if (visited[x][y]) {
+                return sum
+            }
+            if (grid[x][y] == 0) {
+                return sum
+            }
+            visited[x][y] = true
+            var max = 0
+            for (dir in dirs) {
+                max = max(max, calculateWithSumParameter(grid, visited, x + dir[0], y + dir[1], sum + grid[x][y]))
+            }
+            visited[x][y] = false
+            return max
+        }
     }
 }
