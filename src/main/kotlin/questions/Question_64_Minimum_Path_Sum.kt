756c14752fa48bc51abb1705b876c0cf29ccc8df
Andrew Li <ali@bn.com>
2024-09-19 16:19:21 +0800

    Add question 64 another solution

--- src/main/kotlin/questions/Question_64_Minimum_Path_Sum.kt
+++ src/main/kotlin/questions/Question_64_Minimum_Path_Sum.kt
@@ -1,6 +1,5 @@
 package questions
 
-import java.math.BigInteger
 import kotlin.math.min
 
 
@@ -32,6 +31,29 @@ class Question_64_Minimum_Path_Sum {
                 }
             }
             return min[grid.size - 1][grid[0].size - 1]
+        }
+
+        fun minPathSumWithRecursive(grid: Array<IntArray>): Int {
+            val result = Array(grid.size) { IntArray(grid[0].size) { -1 } }
+            val ans = recursive(grid, result, result.size - 1, result[0].size - 1)
+            // println("result = ${result.contentDeepToString()}")
+            return ans
+        }
+
+        fun recursive(grid: Array<IntArray>, result: Array<IntArray>, x: Int, y: Int): Int {
+            if (x < 0 || x > grid.size - 1 || y < 0 || y > grid[0].size - 1) {
+                return 10000000
+            }
+            if (result[x][y] != -1) {
+                return result[x][y]
+            }
+            if (x == 0 && y == 0) {
+                result[x][y] = grid[x][y]
+                return result[x][y]
+            }
+            result[x][y] = min(recursive(grid, result, x, y - 1), recursive(grid, result, x - 1, y)) + grid[x][y]
+            // println("result[$x][$y] = ${result[x][y]}")
+            return result[x][y]
         }
     }
 }
