9c00063448d563292b25ab70f13d8eef2a7205ea
Andrew Li <ali@bn.com>
2024-09-24 22:11:14 +0800

    Add question 120 another solution

--- src/main/kotlin/questions/Question_120_Triangle.kt
+++ src/main/kotlin/questions/Question_120_Triangle.kt
@@ -16,6 +16,34 @@ class Question_120_Triangle {
         }
 
         fun minimumTotal(triangle: List<List<Int>>): Int {
+            val result: MutableList<MutableList<Int>> = mutableListOf()
+            for (count in 0..triangle.size - 1) {
+                result.add(triangle[count].toMutableList())
+            }
+            for (i in 1..result.size - 1) {
+                for (j in 0..result[i].size - 1) {
+                    val dirs = arrayOf(intArrayOf(-1, -1), intArrayOf(-1, 0))
+                    var min = Int.MAX_VALUE
+                    for (dir in dirs) {
+                        val prevX = i + dir[0]
+                        val prevY = j + dir[1]
+                        if (prevX < 0 || prevX > triangle.size - 1 || prevY < 0 || prevY > triangle[i - 1].size - 1) {
+                            continue
+                        }
+                        min = min(min, result[prevX][prevY] + triangle[i][j])
+                    }
+                    result[i][j] = min
+                }
+            }
+            // println("result = $result")
+            var ans = Int.MAX_VALUE
+            for (count in 0..result[result.size - 1].size - 1) {
+                ans = min(ans, result[result.size - 1][count])
+            }
+            return ans
+        }
+
+        fun minimumTotalOldSolution(triangle: List<List<Int>>): Int {
             val tempTriangle: MutableList<MutableList<Int>> = mutableListOf()
             for (count in 1..triangle.size) {
                 tempTriangle.add(MutableList(count) { 0 })
