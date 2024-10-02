a622d378cdec86191c98eee4582efcf680e5bb1b
Andrew Li <ali@bn.com>
2024-10-02 18:08:41 +0800

    Add question 790 another solution

--- src/main/kotlin/questions/Question_790_Domino_and_Tromino_Tiling.kt
+++ src/main/kotlin/questions/Question_790_Domino_and_Tromino_Tiling.kt
@@ -33,5 +33,19 @@ class Question_790_Domino_and_Tromino_Tiling {
             // println("dp = ${dp.contentToString()}")
             return dp[dp.size - 1].toInt()
         }
+
+        fun numTilingsQuicker(n: Int): Int {
+            val mod = 1000000007
+            val dp = Array(n + 1) { 0L }
+            dp[0] = 1
+            dp[1] = 1
+            var current = 0L
+            for (i in 2..dp.size - 1) {
+                dp[i] = (2 * current + dp[i - 1] + dp[i - 2]) % mod
+                current = (current + dp[i - 2]) % mod
+            }
+            // println("dp = ${dp.contentToString()}")
+            return dp[dp.size - 1].toInt()
+        }
     }
 }
