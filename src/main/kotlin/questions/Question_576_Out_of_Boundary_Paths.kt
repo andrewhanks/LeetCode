dcb8254722d7d60102c8e30851e47bbb4b2e10a4
Andrew Li <ali@bn.com>
2024-10-02 10:56:20 +0800

    Add question 576 another solution

--- src/main/kotlin/questions/Question_576_Out_of_Boundary_Paths.kt
+++ src/main/kotlin/questions/Question_576_Out_of_Boundary_Paths.kt
@@ -1,22 +1,59 @@
 package questions
 
+import java.util.*
+
 class Question_576_Out_of_Boundary_Paths {
 
     companion object {
 
         fun runQuestion() {
-            val number1 = 2
-            val number2 = 2
-            val number3 = 2
-            val number4 = 0
-            val number5 = 0
-            val result = findPaths(number1, number2, number3, number4, number5)
+//            Input: m = 2, n = 2, maxMove = 2, startRow = 0, startColumn = 0
+//            Output: 6
+            val m = 2
+            val n = 2
+            val maxMove = 2
+            val startRow = 0
+            val startColumn = 0
+            val result = findPaths(m, n, maxMove, startRow, startColumn)
             println("Question 576: $result")
+        }
+
+        fun findPaths(m: Int, n: Int, maxMove: Int, startRow: Int, startColumn: Int): Int {
+            val mod = 1000000007
+            val dirs = arrayOf(intArrayOf(0, 1), intArrayOf(1, 0), intArrayOf(0, -1), intArrayOf(-1, 0))
+            var ans = 0L
+            val queue: Queue<IntArray> = LinkedList()
+            var dp = MutableList(m) { MutableList(n) { 0L } }
+            dp[startRow][startColumn] = 1
+            queue.add(intArrayOf(startRow, startColumn))
+            for (times in 0..maxMove - 1) {
+                val size = queue.size
+                val newDp = MutableList(m) { MutableList(n) { 0L } }
+                for (count in 0..size - 1) {
+                    val pos = queue.remove()
+                    for (dir in dirs) {
+                        val nextX = pos[0] + dir[0]
+                        val nextY = pos[1] + dir[1]
+                        if (nextX < 0 || nextX > m - 1 || nextY < 0 || nextY > n - 1) {
+                            ans += dp[pos[0]][pos[1]]
+                            ans = ans % mod
+                        } else {
+                            if (newDp[nextX][nextY] == 0L) {
+                                queue.add(intArrayOf(nextX, nextY))
+                            }
+                            newDp[nextX][nextY] += dp[pos[0]][pos[1]]
+                            newDp[nextX][nextY] = newDp[nextX][nextY] % mod
+                        }
+                    }
+                }
+                dp = newDp
+            }
+            return ans.toInt()
         }
 
         var dp: Array<Array<IntArray>> = arrayOf()
         var mod = 1000000007
-        fun findPaths(m: Int, n: Int, maxMove: Int, startRow: Int, startColumn: Int): Int {
+        fun findPathsOldSolution(m: Int, n: Int, maxMove: Int, startRow: Int, startColumn: Int): Int {
             dp = Array(m) { Array(n) { IntArray(maxMove + 1) } }
             for (i in 0 until m) for (j in 0 until n) for (k in 0..maxMove) dp[i][j][k] = -1
             return findPathsAtPosition(m, n, maxMove, startRow, startColumn) % mod
