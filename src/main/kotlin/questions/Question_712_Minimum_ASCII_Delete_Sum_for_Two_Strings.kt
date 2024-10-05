85cd502a616229a69c0d6d7b46e037ebc1b37f8b
Andrew Li <ali@bn.com>
2024-10-05 10:44:29 +0800

    Format code of question 712

--- src/main/kotlin/questions/Question_712_Minimum_ASCII_Delete_Sum_for_Two_Strings.kt
+++ src/main/kotlin/questions/Question_712_Minimum_ASCII_Delete_Sum_for_Two_Strings.kt
@@ -16,25 +16,25 @@ class Question_712_Minimum_ASCII_Delete_Sum_for_Two_Strings {
         }
 
         fun minimumDeleteSum(s1: String, s2: String): Int {
-            val temp1 = "#"+s1
-            val temp2 = "#"+s2
-            val dp = Array(temp1.length){IntArray(temp2.length){0}}
-            for(i in 1..dp.size-1) {
-                dp[i][0] = dp[i-1][0]+temp1[i].toInt()
+            val temp1 = "#" + s1
+            val temp2 = "#" + s2
+            val dp = Array(temp1.length) { IntArray(temp2.length) { 0 } }
+            for (i in 1..dp.size - 1) {
+                dp[i][0] = dp[i - 1][0] + temp1[i].toInt()
             }
-            for(i in 1..dp[0].size-1) {
-                dp[0][i] = dp[0][i-1]+temp2[i].toInt()
+            for (i in 1..dp[0].size - 1) {
+                dp[0][i] = dp[0][i - 1] + temp2[i].toInt()
             }
-            for(i in 1..dp.size-1) {
-                for(j in 1..dp[0].size-1) {
-                    if(temp1[i]==temp2[j]) {
-                        dp[i][j]=dp[i-1][j-1]
-                    }else {
-                        dp[i][j] = min(dp[i-1][j]+temp1[i].toInt(), dp[i][j-1]+temp2[j].toInt())
+            for (i in 1..dp.size - 1) {
+                for (j in 1..dp[0].size - 1) {
+                    if (temp1[i] == temp2[j]) {
+                        dp[i][j] = dp[i - 1][j - 1]
+                    } else {
+                        dp[i][j] = min(dp[i - 1][j] + temp1[i].toInt(), dp[i][j - 1] + temp2[j].toInt())
                     }
                 }
             }
-            return dp[dp.size-1][dp[0].size-1]
+            return dp[dp.size - 1][dp[0].size - 1]
         }
     }
 }
