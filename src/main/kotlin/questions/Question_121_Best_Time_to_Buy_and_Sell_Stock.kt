9c93c9f9cc9893ebce6944fc24f7712dd4921456
Andrew Li <ali@bn.com>
2024-10-08 15:05:44 +0800

    Add question 121 another solution

--- src/main/kotlin/questions/Question_121_Best_Time_to_Buy_and_Sell_Stock.kt
+++ src/main/kotlin/questions/Question_121_Best_Time_to_Buy_and_Sell_Stock.kt
@@ -15,6 +15,23 @@ class Question_121_Best_Time_to_Buy_and_Sell_Stock {
         }
 
         fun maxProfit(prices: IntArray): Int {
+            var min = Int.MAX_VALUE
+            var max = 0
+            var ans = 0
+            for (count in 0..prices.size - 1) {
+                if (prices[count] < min) {
+                    ans = max(ans, max - min)
+                    min = prices[count]
+                    max = min
+                } else {
+                    max = max(max, prices[count])
+                }
+            }
+            ans = max(ans, max - min)
+            return ans
+        }
+
+        fun maxProfitAnotherSolution(prices: IntArray): Int {
             var buy = Int.MIN_VALUE
             var sell = 0
             for (count in 0..prices.size - 1) {
