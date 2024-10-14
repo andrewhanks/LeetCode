56fd1776ca245bf0d3c85379aae03b077177a82f
Andrew Li <ali@bn.com>
2024-10-14 14:13:57 +0800

    Add question 1738 another solution

--- src/main/kotlin/questions/Question_1738_Find_Kth_Largest_XOR_Coordinate_Value.kt
+++ src/main/kotlin/questions/Question_1738_Find_Kth_Largest_XOR_Coordinate_Value.kt
@@ -43,5 +43,52 @@ class Question_1738_Find_Kth_Largest_XOR_Coordinate_Value {
             }
             return ans
         }
+
+        // 1 1 1 X X    2 2 1 X X    2 2 1 X X    3 3 1 X X
+        // 1 1 1 X X    2 2 1 X X    2 2 1 X X    3 3 1 X X
+        // 0 0 0 X X => 1 1 0 X X => 1 1 1 X X => 1 1 1 X X
+        // X X X X X    X X X X X    X X X X X    X X X X X
+        // X X X X X    X X X X X    X X X X X    X X X X X
+        fun kthLargestValueAnotherSolution(matrix: Array<IntArray>, k: Int): Int {
+            val result = Array(matrix.size) { IntArray(matrix[0].size) { 0 } }
+            val queue: PriorityQueue<Int> = PriorityQueue { a, b -> b - a }
+            result[0][0] = matrix[0][0]
+            for (i in 1..result.size - 1) {
+                result[i][0] = result[i - 1][0] xor matrix[i][0]
+            }
+            for (j in 1..result[0].size - 1) {
+                result[0][j] = result[0][j - 1] xor matrix[0][j]
+            }
+            for (i in 1..result.size - 1) {
+                for (j in 1..result[0].size - 1) {
+                    result[i][j] = result[i - 1][j] xor result[i][j - 1] xor matrix[i][j] xor result[i - 1][j - 1]
+                }
+            }
+
+            // binary search
+            var left = 0
+            var right = Int.MAX_VALUE
+            while (left < right) {
+                val mid = right - (right - left) / 2
+                if (count(result, mid) < k) {
+                    right = mid - 1
+                } else {
+                    left = mid
+                }
+            }
+            return left
+        }
+
+        fun count(result: Array<IntArray>, mid: Int): Int {
+            var ans = 0
+            for (i in 0..result.size - 1) {
+                for (j in 0..result[0].size - 1) {
+                    if (result[i][j] >= mid) {
+                        ans++
+                    }
+                }
+            }
+            return ans
+        }
     }
 }
