d8d8f4f79ba2cfc832393a270fda6dcd811083d1
Andrew Li <ali@bn.com>
2024-10-25 16:07:28 +0800

    Add question 2182 another solution

--- src/main/kotlin/questions/Question_2182_Construct_String_With_Repeat_Limit.kt
+++ src/main/kotlin/questions/Question_2182_Construct_String_With_Repeat_Limit.kt
@@ -17,6 +17,47 @@ class Question_2182_Construct_String_With_Repeat_Limit {
         }
 
         fun repeatLimitedString(s: String, repeatLimit: Int): String {
+            val result = IntArray(26) { 0 }
+            for (count in 0..s.length - 1) {
+                result[s[count] - 'a']++
+            }
+            var i = 25
+            var j = i
+            var count = 0
+            var ans = StringBuilder("")
+            while (i >= 0 && j >= 0) {
+                while (i >= 0 && result[i] == 0) {
+                    i--
+                }
+                if (i < 0) {
+                    break
+                }
+                // println("i = $i, j = $j, count = $count, ans = ${ans.toString()}")
+                if (count < repeatLimit) {
+                    ans.append((i + 'a'.toInt()).toChar())
+                    result[i]--
+                    if (result[i] == 0) {
+                        count = 0
+                    } else {
+                        count++
+                    }
+                } else {
+                    j = i - 1
+                    while (j >= 0 && result[j] == 0) {
+                        j--
+                    }
+                    if (j < 0) {
+                        break
+                    }
+                    ans.append((j + 'a'.toInt()).toChar())
+                    result[j]--
+                    count = 0
+                }
+            }
+            return ans.toString()
+        }
+
+        fun repeatLimitedStringSlower(s: String, repeatLimit: Int): String {
             val map: TreeMap<Char, Int> = TreeMap()
             for (count in 0..s.length - 1) {
                 map[s[count]] = map.getOrDefault(s[count], 0) + 1
