3ffe42e3cda2b3811729957142b0a38611daf53b
Andrew Li <ali@bn.com>
2024-10-31 15:19:11 +0800

    Add question 1443 another solution

--- src/main/kotlin/questions/Question_1443_Minimum_Time_to_Collect_All_Apples_in_a_Tree.kt
+++ src/main/kotlin/questions/Question_1443_Minimum_Time_to_Collect_All_Apples_in_a_Tree.kt
@@ -55,5 +55,45 @@ class Question_1443_Minimum_Time_to_Collect_All_Apples_in_a_Tree {
             val ans = 2 * edges.size - 2 * set.size
             return ans
         }
+
+        fun minTimeWithRecursive(n: Int, edges: Array<IntArray>, hasApple: List<Boolean>): Int {
+            val map: MutableMap<Int, MutableList<Int>> = mutableMapOf()
+            for (count in 0..edges.size - 1) {
+                val node1 = edges[count][0]
+                val node2 = edges[count][1]
+                val list1 = map.getOrDefault(node1, mutableListOf())
+                list1.add(node2)
+                map[node1] = list1
+                val list2 = map.getOrDefault(node2, mutableListOf())
+                list2.add(node1)
+                map[node2] = list2
+            }
+            val set: MutableSet<Int> = mutableSetOf()
+            set.add(0)
+            return dfs(0, map, hasApple, set)
+        }
+
+        fun dfs(
+            current: Int,
+            map: MutableMap<Int, MutableList<Int>>,
+            hasApple: List<Boolean>,
+            set: MutableSet<Int>
+        ): Int {
+            if (map[current] == null) {
+                return 0
+            }
+            var totalTime = 0
+            for (item in map[current]!!) {
+                if (set.contains(item)) {
+                    continue
+                }
+                set.add(item)
+                val childTime = dfs(item, map, hasApple, set)
+                if (hasApple[item] || childTime > 0) {
+                    totalTime += childTime + 2
+                }
+            }
+            return totalTime
+        }
     }
 }
