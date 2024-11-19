d019a5d699743bea6cc760cf4848f57938cacd6b
Andrew Li <ali@bn.com>
2024-11-19 20:38:42 +0800

    Add question 2420 another solution

--- src/main/kotlin/questions/Question_2420_Find_All_Good_Indices.kt
+++ src/main/kotlin/questions/Question_2420_Find_All_Good_Indices.kt
@@ -57,5 +57,48 @@ class Question_2420_Find_All_Good_Indices {
             }
             return ans.toList()
         }
+
+        fun goodIndicesFaster(nums: IntArray, k: Int): List<Int> {
+            if (nums.size < 2 * k + 1) {
+                return listOf()
+            }
+            if (k == 1) {
+                val ans: MutableList<Int> = mutableListOf()
+                for (count in 1..nums.size - 2) {
+                    ans.add(count)
+                }
+                return ans.toList()
+            }
+            var nonIncreasingCount = 0
+            var nonDecreasingCount = 0
+            val ans: MutableList<Int> = mutableListOf()
+            for (i in 0..nums.size - k - 3) {
+                if (nums[i + 1] <= nums[i]) {
+                    if (nonIncreasingCount == k) {
+                        nonIncreasingCount--
+                    } else if (nonIncreasingCount == 0) {
+                        nonIncreasingCount = 1
+                    }
+                } else {
+                    nonIncreasingCount = 0
+                }
+                nonIncreasingCount++
+                if (nums[i + k + 2] >= nums[i + k + 1]) {
+                    if (nonDecreasingCount == k) {
+                        nonDecreasingCount--
+                    } else if (nonDecreasingCount == 0) {
+                        nonDecreasingCount = 1
+                    }
+                } else {
+                    nonDecreasingCount = 0
+                }
+                nonDecreasingCount++
+                // println("nonIncreasingCount = $nonIncreasingCount, nonDecreasingCount = $nonDecreasingCount")
+                if (nonIncreasingCount == k && nonDecreasingCount == k) {
+                    ans.add(i + 2)
+                }
+            }
+            return ans.toList()
+        }
     }
 }
