60e9c34cf460682e2b5429527efea151c0be015c
Andrew Li <ali@bn.com>
2024-12-03 14:17:38 +0800

    Add question 792 another solution

--- src/main/kotlin/questions/Question_792_Number_of_Matching_Subsequences.kt
+++ src/main/kotlin/questions/Question_792_Number_of_Matching_Subsequences.kt
@@ -16,14 +16,14 @@ class Question_792_Number_of_Matching_Subsequences {
         fun numMatchingSubseq(s: String, words: Array<String>): Int {
             var ans = 0
             for (item in words) {
-                if (check(s, item)) {
+                if (checkSequentially(s, item)) {
                     ans++
                 }
             }
             return ans
         }
 
-        fun check(s: String, word: String): Boolean {
+        fun checkSequentially(s: String, word: String): Boolean {
             var i = 0
             var j = 0
             while (i <= s.length - 1 && j <= word.length - 1) {
@@ -38,6 +38,39 @@ class Question_792_Number_of_Matching_Subsequences {
                 }
             }
             return false
+        }
+
+        fun numMatchingSubseqBinarySearch(s: String, words: Array<String>): Int {
+            val pos: MutableList<MutableList<Int>> = MutableList(26) { mutableListOf() }
+            for (count in 0..s.length - 1) {
+                pos[s[count] - 'a'].add(count)
+            }
+            var ans = 0
+            for (item in words) {
+                if (checkBinarySearch(pos, item)) {
+                    ans++
+                }
+            }
+            return ans
+        }
+
+        fun checkBinarySearch(pos: MutableList<MutableList<Int>>, word: String): Boolean {
+            var count = 0
+            for (i in 0..word.length - 1) {
+                val target = pos[word[i] - 'a']
+                val insertPoint = target.binarySearch(count)
+                var j = if (insertPoint >= 0) {
+                    insertPoint
+                } else {
+                    -(insertPoint + 1)
+                }
+                // println("word[$i] = ${word[i]}, insertPoint = $insertPoint, j = $j")
+                if (j >= target.size) {
+                    return false
+                }
+                count = target[j] + 1
+            }
+            return true
         }
     }
 }
