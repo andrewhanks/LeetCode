0cb571cf50737efceccb8a9b3ab2d961efb8ef5d
Andrew Li <ali@bn.com>
2024-12-24 17:04:18 +0800

    Add question 1947 another solution

--- src/main/kotlin/questions/Question_1947_Maximum_Compatibility_Score_Sum.kt
+++ src/main/kotlin/questions/Question_1947_Maximum_Compatibility_Score_Sum.kt
@@ -63,5 +63,54 @@ class Question_1947_Maximum_Compatibility_Score_Sum {
             }
             return ans
         }
+
+        fun maxCompatibilitySumByDFS(students: Array<IntArray>, mentors: Array<IntArray>): Int {
+            val size = students.size
+            val number = students[0].size
+            val match = Array(size) { IntArray(size) { 0 } }
+            for (i in 0..size - 1) {
+                for (j in 0..size - 1) {
+                    var count = 0
+                    for (k in 0..number - 1) {
+                        if (students[i][k] == mentors[j][k]) {
+                            count++
+                        }
+                    }
+                    match[i][j] = count
+                }
+            }
+
+            val occupied = Array(size) { false }
+            val result = IntArray(1) { 0 }
+            val temp: MutableList<Int> = mutableListOf()
+            compute(match, result, temp, occupied, 0)
+            return result[0]
+        }
+
+        fun compute(
+            match: Array<IntArray>,
+            result: IntArray,
+            temp: MutableList<Int>,
+            occupied: Array<Boolean>,
+            index: Int
+        ) {
+            if (index >= match.size) {
+                val sum = temp.sum()
+                if (sum > result[0]) {
+                    result[0] = sum
+                }
+                return
+            }
+            for (i in 0..match[index].size - 1) {
+                if (occupied[i]) {
+                    continue
+                }
+                temp.add(match[index][i])
+                occupied[i] = true
+                compute(match, result, temp, occupied, index + 1)
+                temp.removeAt(temp.size - 1)
+                occupied[i] = false
+            }
+        }
     }
 }
