943b27d078e68c0c49bdb5b6e09399a3c50d9e76
Andrew Li <ali@bn.com>
2025-03-28 14:37:04 +0800

    Add question 1079

---
+++ src/main/kotlin/questions/Question_1079_Letter_Tile_Possibilities.kt
@@ -0,0 +1,62 @@
+package questions
+
+import kotlin.math.pow
+
+class Question_1079_Letter_Tile_Possibilities {
+
+    companion object {
+
+        fun runQuestion() {
+//            Input: tiles = "AAB"
+//            Output: 8
+            val s = "AAB"
+            val result = numTilePossibilities(s)
+            println("Question 1079: $result")
+        }
+
+        fun numTilePossibilities(tiles: String): Int {
+            val set: MutableSet<String> = mutableSetOf()
+            val max = (2.0).pow(tiles.length) - 1
+            var ans = 0
+            for (i in 1..max.toInt()) {
+                var selector = Integer.toBinaryString(i)
+                while (selector.length < tiles.length) {
+                    selector = "0" + selector
+                }
+                // println("selector = $selector")
+                var picked = ""
+                for (j in 0..selector.length - 1) {
+                    if (selector[j] == '1') {
+                        picked += tiles[j]
+                    }
+                }
+                picked = String(picked.toCharArray().sorted().toCharArray())
+                if (!set.contains(picked)) {
+                    set.add(picked)
+                    val list = IntArray(26) { 0 }
+                    for (k in 0..picked.length - 1) {
+                        list[picked[k] - 'A']++
+                    }
+                    var times = calculate(picked.length)
+                    for (k in 0..25) {
+                        if (list[k] == 0) {
+                            continue
+                        }
+                        times /= calculate(list[k])
+                    }
+                    // println("picked = $picked, times = $times")
+                    ans += times
+                }
+            }
+            return ans
+        }
+
+        fun calculate(number: Int): Int {
+            var ret = 1
+            for (i in 1..number) {
+                ret *= i
+            }
+            return ret
+        }
+    }
+}
