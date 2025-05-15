416e72a961b4946eb0ac9d63ddb0f4520f453945
Andrew Li <ali@bn.com>
2025-05-15 17:16:35 +0800

    Add question 1191 another solution

--- src/main/kotlin/questions/Question_1191_K_Concatenation_Maximum_Sum.kt
+++ src/main/kotlin/questions/Question_1191_K_Concatenation_Maximum_Sum.kt
@@ -58,5 +58,33 @@ class Question_1191_K_Concatenation_Maximum_Sum {
             val ans = ((sum * k) % mod - minFromLeft - minFromRight) % mod
             return max(0, ans.toInt())
         }
+
+        fun kConcatenationMaxSumAnotherSolution(arr: IntArray, k: Int): Int {
+            val mod = 1000000007
+            val count = if (k == 1) {
+                arr.size - 1
+            } else {
+                arr.size * 2 - 1
+            }
+            var max = 0L
+            var sum = 0L
+            for (i in 0..count) {
+                sum = sum + arr[i % arr.size]
+                max = max(max, sum)
+                if (sum < 0) {
+                    sum = 0
+                }
+            }
+            var total = 0L
+            for (i in 0..arr.size - 1) {
+                total = total + arr[i]
+            }
+            // println("max = $max, total = $total")
+            if (total <= 0L || k <= 2) {
+                return (max % mod).toInt()
+            } else {
+                return ((total * (k - 2) + max) % mod).toInt()
+            }
+        }
     }
 }
