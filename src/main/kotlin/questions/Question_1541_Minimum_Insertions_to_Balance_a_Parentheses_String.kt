6dc1ea905a42438e2b971b299dfd47b650a90129
Andrew Li <ali@bn.com>
2025-05-27 14:19:09 +0800

    Add question 1541 another solution

--- src/main/kotlin/questions/Question_1541_Minimum_Insertions_to_Balance_a_Parentheses_String.kt
+++ src/main/kotlin/questions/Question_1541_Minimum_Insertions_to_Balance_a_Parentheses_String.kt
@@ -47,5 +47,33 @@ class Question_1541_Minimum_Insertions_to_Balance_a_Parentheses_String {
             }
             return ans
         }
+
+        fun minInsertionsByCounting(s: String): Int {
+            var count = 0
+            var ans = 0
+            var i = 0
+            while (i <= s.length - 1) {
+                if (s[i] == '(') {
+                    count++
+                } else {
+                    if (i + 1 <= s.length - 1 && s[i + 1] == ')') {
+                        count--
+                        i++
+                    } else {
+                        ans++
+                        count--
+                    }
+                }
+                if (count < 0) {
+                    ans -= count
+                    count = 0
+                }
+                i++
+            }
+            if (count > 0) {
+                ans += count * 2
+            }
+            return ans
+        }
     }
 }
