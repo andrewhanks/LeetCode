145cd94382d1f21bacb374969b49f11e4fb00dd1
Andrew Li <ali@bn.com>
2025-06-05 11:52:37 +0800

    Add question 948 another solution

--- src/main/kotlin/questions/Question_948_Bag_of_Tokens.kt
+++ src/main/kotlin/questions/Question_948_Bag_of_Tokens.kt
@@ -1,17 +1,45 @@
 package questions
 
+import kotlin.math.max
+
 class Question_948_Bag_of_Tokens {
 
     companion object {
 
         fun runQuestion() {
-            val inputNums = intArrayOf(100, 200, 300, 400)
-            val inputTarget = 200
-            val result = bagOfTokensScore(inputNums, inputTarget)
+//            Input: tokens = [100,200,300,400], power = 200
+//            Output: 2
+            val tokens = intArrayOf(100, 200, 300, 400)
+            val power = 200
+            val result = bagOfTokensScore(tokens, power)
             println("Question 948: $result")
         }
 
         fun bagOfTokensScore(tokens: IntArray, power: Int): Int {
+            tokens.sort()
+            var i = 0
+            var j = tokens.size - 1
+            var score = 0
+            var temp = power
+            var max = 0
+            while (i <= j) {
+                if (temp >= tokens[i]) {
+                    temp -= tokens[i]
+                    score++
+                    max = max(max, score)
+                    i++
+                } else if (score > 0) {
+                    score--
+                    temp += tokens[j]
+                    j--
+                } else {
+                    break
+                }
+            }
+            return max
+        }
+
+        fun bagOfTokensScoreOldSolution(tokens: IntArray, power: Int): Int {
             tokens.sort()
             var lastAddPowerPosition = tokens.size - 1
             var tempPower = power
