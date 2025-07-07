98656c27700ef5519b02706afe82ad14f50fe8ff
Andrew Li <ali@bn.com>
2025-07-07 16:06:22 +0800

    Add question 787 another solution

--- src/main/kotlin/questions/Question_787_Cheapest_Flights_Within_K_Stops.kt
+++ src/main/kotlin/questions/Question_787_Cheapest_Flights_Within_K_Stops.kt
@@ -1,5 +1,6 @@
 package questions
 
+import java.util.*
 import kotlin.math.min
 
 
@@ -43,6 +44,38 @@ class Question_787_Cheapest_Flights_Within_K_Stops {
             } else {
                 return result[dst]
             }
+        }
+
+        fun findCheapestPriceByDijkstraAlgorithm(n: Int, flights: Array<IntArray>, src: Int, dst: Int, k: Int): Int {
+            val map: MutableMap<Int, MutableList<IntArray>> = mutableMapOf()
+            for (flight in flights) {
+                map.putIfAbsent(flight[0], mutableListOf())
+                map[flight[0]]!!.add(intArrayOf(flight[1], flight[2]))
+            }
+            val price = Array(n) { IntArray(k + 2) { Int.MAX_VALUE / 2 } }
+            val queue = PriorityQueue<IntArray> { a, b ->
+                a[1] - b[1]
+            }
+            queue.add(intArrayOf(src, 0, 0)) // current, price, times
+            while (!queue.isEmpty()) {
+                val node = queue.remove()
+                if (node[0] == dst) {
+                    return node[1]
+                }
+                if (node[2] > k) {
+                    continue
+                }
+                if (price[node[0]][node[2] + 1] < Int.MAX_VALUE / 2) {
+                    continue
+                }
+                price[node[0]][node[2] + 1] = node[1]
+                if (map[node[0]] != null) {
+                    for (item in map[node[0]]!!) {
+                        queue.add(intArrayOf(item[0], node[1] + item[1], node[2] + 1))
+                    }
+                }
+            }
+            return -1
         }
     }
 }
