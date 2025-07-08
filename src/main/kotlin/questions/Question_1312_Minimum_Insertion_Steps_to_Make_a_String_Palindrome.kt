9e8f6cb82a50f5bc6b4a86ca8297e4a0f75e3cf1
Andrew Li <ali@bn.com>
2025-07-08 13:29:53 +0800

    Add question 1312 another solution

--- src/main/kotlin/questions/Question_1312_Minimum_Insertion_Steps_to_Make_a_String_Palindrome.kt
+++ src/main/kotlin/questions/Question_1312_Minimum_Insertion_Steps_to_Make_a_String_Palindrome.kt
@@ -1,6 +1,7 @@
 package questions
 
 import kotlin.math.max
+import kotlin.math.min
 
 class Question_1312_Minimum_Insertion_Steps_to_Make_a_String_Palindrome {
 
@@ -14,6 +15,7 @@ class Question_1312_Minimum_Insertion_Steps_to_Make_a_String_Palindrome {
             println("Question 1312: $result")
         }
 
+        // s.length - LCS
         fun minInsertions(s: String): Int {
             val len = s.length
             val revS = s.reversed()
@@ -29,6 +31,37 @@ class Question_1312_Minimum_Insertion_Steps_to_Make_a_String_Palindrome {
             }
             // println("dp = ${dp.contentDeepToString()}")
             return s.length - dp[len][len]
+        }
+
+        fun minInsertionsByDpStringItself(s: String): Int {
+            val m = s.length
+            val dp = Array(m) { IntArray(m) { 0 } }
+            for (i in 0..m - 1) {
+                dp[i][i] = 0
+            }
+            for (i in 0..m - 2) {
+                dp[i][i + 1] = if (s[i] == s[i + 1]) {
+                    0
+                } else {
+                    1
+                }
+            }
+            for (len in 3..m) {
+                for (i in 0..m - len) {
+                    val j = i + len - 1
+                    // println("i = $i, j = $j, len = $len")
+                    if (s[i] == s[j]) {
+                        // iXXXXXXXXj
+                        dp[i][j] = dp[i + 1][j - 1]
+                    } else {
+                        // min ( iXXXXXXXXji , jiXXXXXXXXj )
+                        //                 +   +
+                        dp[i][j] = min(dp[i + 1][j] + 1, dp[i][j - 1] + 1)
+                    }
+                }
+            }
+            // println("dp = ${dp.contentDeepToString()}")
+            return dp[0][m - 1]
         }
     }
 }
