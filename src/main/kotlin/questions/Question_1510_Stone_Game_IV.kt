9e6092c1476fc2580fd59efe08ad54077a6ae0d3
Andrew Li <ali@bn.com>
2025-07-14 18:20:51 +0800

    Add question 1510

---
+++ src/main/kotlin/questions/Question_1510_Stone_Game_IV.kt
@@ -0,0 +1,31 @@
+package questions
+
+class Question_1510_Stone_Game_IV {
+
+    companion object {
+
+        fun runQuestion() {
+//            Input: n = 4
+//            Output: true
+            val n = 4
+            val result = winnerSquareGame(n)
+            println("Question 1510: $result")
+        }
+
+        fun winnerSquareGame(n: Int): Boolean {
+            val dp = Array(n + 1) { false }
+            dp[0] = false
+            for (i in 1..n) {
+                var temp = 1
+                while (temp * temp <= i) {
+                    if (!dp[i - temp * temp]) {
+                        dp[i] = true
+                        break
+                    }
+                    temp++
+                }
+            }
+            return dp[n]
+        }
+    }
+}
