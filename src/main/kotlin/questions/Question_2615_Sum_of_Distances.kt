debb419a85220b114694363e80b801c08d578578
Andrew Li <ali@bn.com>
2025-08-06 16:46:19 +0800

    Add question 2615 another solution

--- src/main/kotlin/questions/Question_2615_Sum_of_Distances.kt
+++ src/main/kotlin/questions/Question_2615_Sum_of_Distances.kt
@@ -1,5 +1,7 @@
 package questions
 
+import kotlin.math.abs
+
 class Question_2615_Sum_of_Distances {
 
     companion object {
@@ -35,6 +37,41 @@ class Question_2615_Sum_of_Distances {
                     val iLong = i.toLong()
                     acc += index
                     ans[index] = (iLong + 1) * index - acc - (list.size - iLong - 1) * index + (total - acc)
+                }
+            }
+            return ans
+        }
+
+        // list: X X X i X X X X X
+        //      +d+d+d+d
+        //              -d-d-d-d-d
+        // s[i] = sum( abs( xi - xj ) ), for j=0,1,2,...,size-1
+        // val d = list[i] - list[i-1]
+        // s[i] = s[i-1] + (i)*d - (size-i)*d
+        fun distanceAnotherSolution(nums: IntArray): LongArray {
+            val map: MutableMap<Int, MutableList<Int>> = mutableMapOf()
+            val ans = LongArray(nums.size) { 0L }
+            for (i in 0..nums.size - 1) {
+                map.putIfAbsent(nums[i], mutableListOf())
+                map[nums[i]]!!.add(i)
+            }
+            for ((key, list) in map) {
+                if (list.size == 1) {
+                    continue
+                }
+                var sum = 0L
+                for (i in 0..list.size - 1) {
+                    val index = list[i]
+                    if (i == 0) {
+                        for (j in 0..list.size - 1) {
+                            sum += abs(index - list[j])
+                        }
+                    } else {
+                        val iLong = i.toLong()
+                        val d = list[i] - list[i - 1]
+                        sum += (iLong) * d - (list.size - iLong) * d
+                    }
+                    ans[index] = sum
                 }
             }
             return ans
