cf6414cbe1e61182d2ca0a04ca63d1c1f090f8d5
Andrew Li <ali@bn.com>
2025-08-12 13:31:25 +0800

    Add question 1653 another solution

--- src/main/kotlin/questions/Question_1653_Minimum_Deletions_to_Make_String_Balanced.kt
+++ src/main/kotlin/questions/Question_1653_Minimum_Deletions_to_Make_String_Balanced.kt
@@ -48,5 +48,37 @@ class Question_1653_Minimum_Deletions_to_Make_String_Balanced {
             }
             return ans
         }
+
+        fun minimumDeletionsAnotherSolution(s: String): Int {
+            val n = s.length
+            val changeToA = IntArray(n) { 0 }
+            val changeToB = IntArray(n) { 0 }
+            if (s[0] == 'b') {
+                changeToA[0] = 1
+            }
+            for (i in 1..n - 1) {
+                if (s[i] == 'b') {
+                    changeToA[i] = changeToA[i - 1] + 1
+                } else {
+                    changeToA[i] = changeToA[i - 1]
+                }
+            }
+            if (s[n - 1] == 'a') {
+                changeToB[n - 1] = 1
+            }
+            for (i in n - 2 downTo 0) {
+                if (s[i] == 'a') {
+                    changeToB[i] = changeToB[i + 1] + 1
+                } else {
+                    changeToB[i] = changeToB[i + 1]
+                }
+            }
+            // println("changeToA = ${changeToA.contentToString()}, changeToB = ${changeToB.contentToString()}")
+            var ans = min(changeToA[n - 1], changeToB[0])
+            for (i in 0..n - 2) {
+                ans = min(ans, changeToA[i] + changeToB[i + 1])
+            }
+            return ans
+        }
     }
 }
